a1a16b986175ee4bf5b575749574b0bfb0da74ee
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2024-03-22 11:46:33 +0100

    changed setup.rb

--- setup.rb
+++ setup.rb
@@ -1,7 +1,11 @@
 puts "Setup submodules"
+system("git submodule update --init --recursive --checkout")
 system('git submodule foreach --recursive git submodule init')
 system('git submodule foreach --recursive git submodule update')
 system('git submodule foreach --recursive git submodule sync')
+system('(cd cmake_utils; git checkout main)')
+system('(cd submodules/Utils; git checkout master)')
+system('(cd submodules/Utils/cmake_utils; git checkout main)')
 
 puts ARGV
 
