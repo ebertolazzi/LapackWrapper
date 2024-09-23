c45e7b82c5b73c1d76f07d6d7d865910aed7972d
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2024-09-23 22:07:32 +0200

    updated UtilsLite

--- setup.rb
+++ setup.rb
@@ -4,8 +4,8 @@ system('git submodule foreach --recursive git submodule init')
 system('git submodule foreach --recursive git submodule update')
 system('git submodule foreach --recursive git submodule sync')
 system('(cd cmake_utils; git checkout main)')
-system('(cd submodules/Utils; git checkout master)')
-system('(cd submodules/Utils/cmake_utils; git checkout main)')
+system('(cd submodules/UtilsLite; git checkout master)')
+system('(cd submodules/UtilsLite/cmake_utils; git checkout main)')
 
 puts ARGV
 
