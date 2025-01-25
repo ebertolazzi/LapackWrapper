cd03121ac4edd1d311331cefb70c4a62ca6acba4
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2025-01-25 23:32:17 +0100

    updated submodules and setup.rb

--- setup.rb
+++ setup.rb
@@ -1,11 +1,23 @@
+require 'yaml'
+
 puts "Setup submodules"
-system("git submodule update --init --recursive --checkout")
+system('git submodule init')
+system('git submodule update')
+system('git submodule sync')
 system('git submodule foreach --recursive git submodule init')
 system('git submodule foreach --recursive git submodule update')
 system('git submodule foreach --recursive git submodule sync')
-system('(cd cmake_utils; git checkout main)')
-system('(cd submodules/UtilsLite; git checkout master)')
-system('(cd submodules/UtilsLite/cmake_utils; git checkout main)')
+
+branches = YAML.load_file("./sub_branches.yaml")
+branches.each do |dir, branch|
+  puts "\n\nChecking out branch #{branch} in #{dir}"
+  if branch.include? ":" then
+    res = branch.split(':');
+    system("(cd #{dir} && git reset --hard && git fetch --tags && git checkout '#{res[1]}' )")
+  else
+    system("(cd #{dir} && git reset --hard && git checkout #{branch} && git pull && git reset --hard)")
+  end
+end
 
 puts ARGV
 
@@ -13,3 +25,5 @@ if ARGV.size() > 0 && ARGV[0] == "--last" then
   puts "\nUpdate submodules to last version"
   system('git submodule foreach --recursive git pull')
 end
+
+system('ruby submodules/UtilsLite/setup.rb')
